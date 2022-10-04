a9605365d8f8bcd175ddbff48766b27fbb8e60bf
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-04 15:02:16 +0200

    Create escape_sequences.py
    using back slashes, quotes, tabs or new lines

---
+++ escape_sequences.py
@@ -0,0 +1,15 @@
+# using tab in print statement:
+print("here \t I put tab and here \t again")
+
+# using new line in print statement:
+print("here I put a \n new line and here I do it again \n new line")
+
+# here I use quotes:
+print("I try to use 'single quotes' and then I use \"double quotes\"")
+# the same result:
+print('I try to use \'single quotes\' and then I use "double quotes"')
+# it works here as well
+print('I try to use \'single quotes\' and then I use \"double quotes\"')
+
+# printing back slashes:
+print("If i want to print \\ I have to add two back slashes.")

4dafaa8f17f216a229f3dc873cd0c74e9cb55006
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-04 15:09:18 +0200

    Create sequences_exercixes.py
    added some examples from lecture

---
+++ sequences_exercixes.py
@@ -0,0 +1,16 @@
+# creating float variable:
+var_float = 1.11
+
+# printing its type:
+print(type(var_float))
+
+# using str() for converting the variable into string:
+var_float = str(var_float)
+
+# printing it with concatenating string as well:
+print(var_float + " is a float")
+
+# printing string sentence with a quotes:
+name = "Tomas"
+print('\"Hello, I\'m ' + name + ', it\'s nice to meet you!\"')
+

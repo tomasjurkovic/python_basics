3128c16fb9f0a13d4f73df0e7b63c9bc3d546590
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-04 15:24:28 +0200

    trying simple inputs

---
+++ inputs.py
@@ -0,0 +1,8 @@
+# inserting an input:
+name = input("Please enter your name: ")
+
+# displaying input in print statement:
+print("Your name is: " + name + ".")
+
+# print name's type (it's always string here):
+print(type(name))
---
+++ monty_python.py
@@ -0,0 +1,7 @@
+name = input("What is your name?\n")
+quest = input("What is your quest?\n")
+fav_color = input("What is your favourite color?\n")
+
+print("So your name is " + name + ", and your quest is " + quest + ", and your favourite color is " + fav_color + ".")
+
+

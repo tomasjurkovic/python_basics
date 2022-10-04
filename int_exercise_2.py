849ba588adac9e8e59cc9874178be795f35a5413
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-04 16:05:41 +0200

    Create int_exercise_2.py

---
+++ int_exercise_2.py
@@ -0,0 +1,10 @@
+user_integer = int(input("Insert value: "))
+
+# I added 10 to user's input:
+final_result = user_integer + 10
+
+# here I print it:
+# some of possible solutions
+print("When I add 10 to that number, than the result is: " + str(user_integer + 10))
+print(final_result)
+print(user_integer + 10)

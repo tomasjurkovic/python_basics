540b3071fa8bb0ee0b0deba09d9f8e188dcb3d07
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-05 11:20:06 +0200

    Update grades_exercise.py
    other solution

--- flow_control/grades_exercise.py
+++ flow_control/grades_exercise.py
@@ -1,17 +1,15 @@
-student_score = int(input("Insert student's score from the exam: "))
+score = int(input("Please enter the student's score. "))
 
-if 60 >= student_score <= 100:
-    if 70 > student_score >= 60:
-        print("The student will receive an D grade.")
-    if 80 > student_score >= 70:
-        print("The student will receive an C grade.")
-    if 90 > student_score >= 80:
-        print("The student will receive an B grade.")
-    if 90 >= student_score:
-        print("The student will receive an A grade.")
+if score >= 90:
+    print("This student's score of " + str(score) + " is an A.")
 else:
-    if student_score < 0 or student_score > 100:
-        print("You inserted invalid value. Students' grades can be only from 0 to 100.")
+    if score >= 80:
+        print("This student's score of " + str(score) + " is a B.")
     else:
-        print("The student will receive an F grade.")
-
+        if score >= 70:
+            print("This student's score of " + str(score) + " is a C.")
+        else:
+            if score >= 60:
+                print("This student's score of " + str(score) + " is a D.")
+            else:
+                print("This student's score of " + str(score) + " is a F.")

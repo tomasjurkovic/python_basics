18de88865be91263287a87178a155d3f14adf0c3
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-05 12:49:16 +0200

    Create roman_numeral_equivalent.py
    basic example of using elif else and if

---
+++ flow_control/roman_numeral_equivalent.py
@@ -0,0 +1,26 @@
+from random import randint
+random_number = randint(1, 10)
+
+if random_number == 1:
+    print("Roman equivalent for " + str(random_number) + " is: I")
+elif random_number == 2:
+    print("Roman equivalent for " + str(random_number) + " is: II")
+elif random_number == 3:
+    print("Roman equivalent for " + str(random_number) + " is: III")
+elif random_number == 4:
+    print("Roman equivalent for " + str(random_number) + " is: IV")
+elif random_number == 5:
+    print("Roman equivalent for " + str(random_number) + " is: V")
+elif random_number == 6:
+    print("Roman equivalent for " + str(random_number) + " is: VI")
+elif random_number == 7:
+    print("Roman equivalent for " + str(random_number) + " is: VII")
+elif random_number == 8:
+    print("Roman equivalent for " + str(random_number) + " is: VIII")
+elif random_number == 9:
+    print("Roman equivalent for " + str(random_number) + " is: IX")
+else:
+    print("Roman equivalent for " + str(random_number) + " is: X")
+
+
+

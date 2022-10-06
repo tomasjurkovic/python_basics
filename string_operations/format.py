71c77081c63eedd2924796412f4e34faf4654c3e
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-06 12:52:20 +0200

    Create format.py
    added example of  using format method

---
+++ string_operations/format.py
@@ -0,0 +1,6 @@
+name = input("Please insert your name: \n")
+degree = input("What did you major in at collage? \n")
+job = input("What is your current occupation? \n")
+experience = input("How many years have you been working in your current job's position? \n")
+
+print("{} majored in {}, works as a {} and has {} years of experience".format(name, degree, job, experience))

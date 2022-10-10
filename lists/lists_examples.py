8978e2dcd151e40871f50db73c1c0b4393969b52
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-10 09:19:33 +0200

    Create lists_examples.py

---
+++ lists/lists_examples.py
@@ -0,0 +1,9 @@
+examples_list_1 = [1, 2, 4, 5, 3]  # list of integers
+examples_list_2 = [1.11, 7.62, 7.42, 1.05, 3.73]  # list of floats
+examples_list_3 = ["Here", "are", "some", "strings" "in list"]  # list of strings
+examples_list_4 = [True, False, True, True, False]  # list of booleans
+examples_list_5 = [[1, 2], [7, 4], [5, 3, 4]]  # list of lists
+example_of_mixed_list = ["String", 1, True, 2.22, [12, 1.2, "True", False], 1]  # list of everything together
+
+print(list("characters"))
+# prints ['c', 'h', 'a', 'r', 'a', 'c', 't', 'e', 'r', 's']

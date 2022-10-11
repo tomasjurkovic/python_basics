694857f32130474c5964a59e55fcfbe21f18b140
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-11 10:30:41 +0200

    added examples for insert and append functions

---
+++ lists/append.py
@@ -0,0 +1,4 @@
+pets = ["cat", "dog", "parrot"]
+print(pets)
+pets.append("fish")  # adds 'fish' at the end of the list
+print(pets)  # prints ['cat', 'dog', 'parrot', 'fish']
---
+++ lists/insert.py
@@ -0,0 +1,3 @@
+pets = ["cat", "dog", "parrot"]
+pets.insert(1, "turtle")  # it adds new item anywhere to the list - I chose index 1 position in the first argument
+print(pets)  # prints ["cat", "turtle", "dog", "parrot"]

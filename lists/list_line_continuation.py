3e29d682e977e3eef25ea171f4c27c8e6deed4c4
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-14 14:16:59 +0200

    Create list_line_continuation.py

---
+++ lists/list_line_continuation.py
@@ -0,0 +1,26 @@
+list_of_strings = ["One",
+                   "two",
+                   "three",
+                   "this is how it should be"]
+
+print(list_of_strings)
+
+# using of line continuation character "\" in math expression
+
+ex_int = 2 + \
+       4 + \
+       1
+
+print(ex_int)  # prints 7
+
+# using of line continuation character "\"
+ex_string = "The Emp\
+ire Strikes \
+Back"
+
+print(ex_string)  # prints "The Empire Strikes Back" in string
+
+ex_string_plus = "hello " + \
+                 "world"
+
+print(ex_string_plus)  # prints "hello world"

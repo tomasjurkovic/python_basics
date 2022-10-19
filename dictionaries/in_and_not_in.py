c0fa815794f9163288bc3998d3ed7f4c4ae2262d
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-19 11:11:02 +0200

    Update in_and_not_in.py
    it's possible to use in method also for checking values

--- dictionaries/in_and_not_in.py
+++ dictionaries/in_and_not_in.py
@@ -7,3 +7,8 @@ print(7 in dict_one)  # prints False, because 7 key does not exist in dict_one d
 # not in operator:
 print(3 not in dict_one)  # prints False, because 3 key exists in dict_one dictionary
 print(7 not in dict_one)  # prints True, because 7 key does not exist in dict_one dictionary
+
+# this is how we use it in values:
+player_numbers = {1: "Aaron", 2: "Kieran", 3: "Benjamin", 5: "Thomas", 6: "Gabriel", 7: "Bukayo"}
+print("Gabriel" in player_numbers.values())
+# prints True, because this value is there

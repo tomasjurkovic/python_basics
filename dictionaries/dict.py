78c5d5d3524b814718bc4f1789f42e291dbbbffd
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-19 14:52:50 +0200

    Create dict.py

---
+++ dictionaries/dict.py
@@ -0,0 +1,7 @@
+empty = dict()  # it creates empty dictionary
+print(empty)  # prints {}
+
+# also it works if I insert key and values like in example
+# then the dictionary will be created
+with_values = dict(value_1=1, value_2=2, value_3=3)
+print(with_values)  # prints {'value_1': 1, 'value_2': 2, 'value_3': 3}

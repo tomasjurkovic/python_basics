8b979c28ad09682a5630addef7ff3c853324443f
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-19 10:50:50 +0200

    Create items.py

---
+++ dictionaries/items.py
@@ -0,0 +1,15 @@
+player_numbers = {1: "Aaron", 2: "Kieran", 3: "Benjamin", 5: "Thomas", 6: "Gabriel", 7: "Bukayo"}
+
+print(player_numbers.items())
+# prints dict_items([(1, 'Aaron'), (2, 'Kieran'), (3, 'Benjamin'), (5, 'Thomas'), (6, 'Gabriel'), (7, 'Bukayo')])
+
+for item in player_numbers.items():
+    print(item)
+
+# prints:
+# (1, 'Aaron')
+# (2, 'Kieran')
+# (3, 'Benjamin')
+# (5, 'Thomas')
+# (6, 'Gabriel')
+# (7, 'Bukayo')

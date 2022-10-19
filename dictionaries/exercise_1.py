c78d371a95858173523fec15485a550b35b9d155
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-19 10:29:22 +0200

    Create exercise_1.py
    where I used knowledge from past lections

---
+++ dictionaries/exercise_1.py
@@ -0,0 +1,11 @@
+# create a variable and assign it a dictionary that has 5 key value pairs
+example_dict = {"name": "Tomas", "age": 28, "country": "Slovakia", "city": "Kosice", "mode": "learning"}
+
+# print and access the value held at the third key in the dictionary
+print(example_dict["country"])  # prints Slovakia
+
+# use the in keyword to check if a key appears in the dictionary and print the result
+print("city" in example_dict)  # prints True
+
+# use not in to check if a key does not appear in the dictionary and print the result
+print("address" not in example_dict)  # prints True

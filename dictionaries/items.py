77823e502a4a4fd3d573091cdb2c10f24098c57a
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-19 10:57:02 +0200

    Update items.py

--- dictionaries/items.py
+++ dictionaries/items.py
@@ -13,3 +13,15 @@ for item in player_numbers.items():
 # (5, 'Thomas')
 # (6, 'Gabriel')
 # (7, 'Bukayo')
+
+# better looking:
+for key, value in player_numbers.items():
+    print(key, value)
+
+# prints:
+# 1 Aaron
+# 2 Kieran
+# 3 Benjamin
+# 5 Thomas
+# 6 Gabriel
+# 7 Bukayo

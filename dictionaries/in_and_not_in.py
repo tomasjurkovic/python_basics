5f36d7e5a5ca8d33be5dd11c4df4e0f265631933
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-19 10:24:12 +0200

    Create in_and_not_in.py

---
+++ dictionaries/in_and_not_in.py
@@ -0,0 +1,9 @@
+dict_one = {0: 100, 1: 444, 2: 647, 3: 363}
+
+# in operator:
+print(3 in dict_one)  # prints True, because 3 key exists in dict_one dictionary
+print(7 in dict_one)  # prints False, because 7 key does not exist in dict_one dictionary
+
+# not in operator:
+print(3 not in dict_one)  # prints False, because 3 key exists in dict_one dictionary
+print(7 not in dict_one)  # prints True, because 7 key does not exist in dict_one dictionary

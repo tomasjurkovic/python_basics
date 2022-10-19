67b731747cf1e9d73691948715efa9bd7384d9b5
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-19 13:54:08 +0200

    Create clear.py

---
+++ dictionaries/clear.py
@@ -0,0 +1,7 @@
+united_kingdom = {1: "England", 2: "Scotland", 3: "Wales", 4: "Northern Ireland"}
+print(united_kingdom)
+
+# clear method takes no argument and clears whole dictionary and only {} remains
+united_kingdom.clear()
+print(united_kingdom)
+# it prints {}

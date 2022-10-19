7f49767d846e56444369e0696bbd61f3aafb53c9
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-19 11:25:48 +0200

    Create more_readable.py

---
+++ dictionaries/more_readable.py
@@ -0,0 +1,10 @@
+player_numbers = {1: 'Aaron',
+     2: 'Kieran',
+     3: 'Benjamin',
+     5: 'Thomas',
+     6: 'Gabriel',
+     7: 'Bukayo',
+     8: 'Martin'}
+
+print(len(player_numbers))
+# it prints 7, becauese it contains 7 key value pairs

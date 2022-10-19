730bcf70529e15fc07d7d604383223b2161a55c8
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-19 10:44:15 +0200

    Create keys.py

---
+++ dictionaries/keys.py
@@ -0,0 +1,16 @@
+player_numbers = {1: "Aaron", 2: "Kieran", 3: "Benjamin", 5: "Thomas", 6: "Gabriel", 7: "Bukayo"}
+
+print(player_numbers.keys())
+# prints dict_keys([1, 2, 3, 5, 6, 7])
+# it prints all keys from the selected dictionary
+
+for key in player_numbers.keys():
+    print(key)
+
+# prints:
+# 1
+# 2
+# 3
+# 5
+# 6
+# 7

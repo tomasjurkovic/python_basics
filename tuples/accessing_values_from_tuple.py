a4dd205cb72b9ce153f282fef9bf5ce678b57d96
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-20 10:13:49 +0200

    Create accessing_values_from_tuple.py
    slice and access print methods

---
+++ tuples/accessing_values_from_tuple.py
@@ -0,0 +1,7 @@
+tuple_6 = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(tuple_6[2])  # prints 3, because it access the 2. index -> 0, 1, 2...
+
+# we can use slice to print values from tuple as well:
+print(tuple_6[:8])  # prints (1, 2, 3, 4, 5, 6, 7, 8)
+print(tuple_6[2:7])  # prints (3, 4, 5, 6, 7)
+print(tuple_6[3:])  # prints (4, 5, 6, 7, 8, 9, 10)
